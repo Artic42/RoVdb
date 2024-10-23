667e43814303b1aa68ce8148df5ff16257827852
Artic42 <main@artic42.com>
2024-10-23 09:50:25 +0200

    Last changes realted to event management

--- database/routers/transmissor.py
+++ database/routers/transmissor.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.responses import JSONResponse
+import databaseManager.eventActions as eventActions
 import os
 
 
@@ -10,6 +11,8 @@ router = APIRouter()
 @router.put("/jammer/activate")
 def jammerArtivate(request: Request):
     os.system("touch /jammerActivated")
+    eventActions.editRedirectID("/Database/Database.db", "12001", "12003")
+    eventActions.editRedirectID("/Database/Database.db", "12101", "12103")
     return JSONResponse({
         "status": 200,
         "message": "Jammer activated"
@@ -19,6 +22,8 @@ def jammerArtivate(request: Request):
 @router.put("/jammer/deactivate")
 def jammerDeactivate(request: Request):
     os.system("rm /jammerActivated")
+    eventActions.editRedirectID("/Database/Database.db", "12001", "12002")
+    eventActions.editRedirectID("/Database/Database.db", "12101", "12102")
     return JSONResponse({
         "status": 200,
         "message": "Jammer deactivated"
